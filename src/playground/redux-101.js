0a0267f7113e6c309c4fbe5479da9213987e4e6a
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-18 19:57:51 -0600

    vid86PART1: wanted to commit right now, just to save how we went over redux. about to use redux the right way, using a switch statement....but this was a good setup. part2 coming next....

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,35 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+// creating a store. it needs at least one argument, which is a function that gets called right away.
+// the second argument is the action object
+const store = createStore((state = { count: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      count: state.count + 1
+    };
+  } else {
+    return state;
+  }
 });
+
+console.log(store.getState());
+// actions - an object that gets sent to the store. this is how you change state in the store
+
+// I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+
+// I'd like to decrement the count
+
+
+// I'd like to reset the count to zero
+
+
 
 console.log(store.getState());
