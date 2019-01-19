eeb3ace3561c57dbe51a3d1fe4c339510152279a
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-18 20:06:34 -0600

    vid86: set up redux actions the right way, but still have it all in one file

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,21 @@ import { createStore } from 'redux';
 // creating a store. it needs at least one argument, which is a function that gets called right away.
 // the second argument is the action object
 const store = createStore((state = { count: 0 }, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      count: state.count + 1
-    };
-  } else {
-    return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: state.count = 0
+      }
+    default:
+      return state;
   }
 });
 
@@ -20,16 +29,19 @@ store.dispatch({
   type: 'INCREMENT'
 });
 
+// I'd like to decrement the count
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'DECREMENT'
 });
 
-
-// I'd like to decrement the count
-
+store.dispatch({
+  type: 'DECREMENT'
+});
 
 // I'd like to reset the count to zero
-
+store.dispatch({
+  type: 'RESET'
+});
 
 
 console.log(store.getState());
