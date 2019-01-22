9bed03efc737ec8edcd96b2aba48e83afad16234
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-21 22:04:12 -0600

    vid99: IF YOU WANT TO SEE HOW TO BREAK UP ONE REDUX FILE INTO MANY COMPONENTS.....THIS IS IT.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import * as expenseActions from './actions/expenses';
+import * as filterActions from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+// dispatch actions to the store
+store.dispatch(expenseActions.addExpense({ description: 'Water bill', note: 'this was late', amount: 500000, createdAt: 30000022323 }));
+store.dispatch(expenseActions.addExpense({ description: 'Gas bill', amount: 4000000, note: 'on time' }));
+//store.dispatch(filterActions.setTextFilter('bill'));   // this returns two bills
+store.dispatch(filterActions.setTextFilter('water'));    // ....but this returns only one
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
---
+++ src/reducers/expenses.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+// ********************************************************************************************* //
+// break up the app into multiple reducers.
+// use a single reducer for each single property in the redux store (expenses, filters).
+// each one will act like the other doesn't exist
+
+// ***** EXPENSES REDUCER **********
+// state starts out as an empty array if there are no expenses
+// SPREAD OPERATOR -> ...state is the array of the 3 original things, I can then add action.expense and it
+// ...it just adds it to the end of the array. this does the same thing as array.concat
+
+const expensesRedducerDefaultState = [];
+
+const expensesReducer = (state = expensesRedducerDefaultState, action) => {
+
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      // console.log('yooo', action.expense)
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      console.log('what does state look like', state)
+      return state.filter(expense => expense.id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        // if my list of expense IDs in state (expense.id) has an ID matching the ID 
+        // that was passed in (action.id), spread out the expense object, and spread out and run 
+        // all of the action.updates
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        };
+      });
+    default:
+      return state;
+  }
+};
+
+export default expensesReducer;
