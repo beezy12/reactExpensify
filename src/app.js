497f5b2c1b15469e3c8a3356ca314dcf11bcfd4d
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-22 22:40:33 -0600

    vid103: can delete an expense now....data does not persist

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ const store = configureStore();
 // dispatch actions to the store
 store.dispatch(expenseActions.addExpense({ description: 'Water bill', note: 'this was late', amount: 500000, createdAt: 302323 }));
 store.dispatch(expenseActions.addExpense({ description: 'Gas bill', amount: 4000000, note: 'on time' }));
-//store.dispatch(filterActions.setTextFilter('bill'));   // this returns two bills
+// store.dispatch(filterActions.setTextFilter('bill'));   // this returns two bills
 store.dispatch(filterActions.setTextFilter('water'));    // ....but this returns only one
 
 setTimeout(() => {
-  store.dispatch(filterActions.setTextFilter('water'));
+  store.dispatch(filterActions.setTextFilter('bill'));
 }, 3000)
 
 const state = store.getState();
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt, note }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt, note }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
     <p>{note}</p>
+    <p>id here {id}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }));
+    }}>REMOVE EXPENSE</button>
   </div>
 );
 
-export default ExpenseListItem;
+// dont have to mapStateToProps here because I just need access to the 'dispatch' prop, which this gives me
+export default connect()(ExpenseListItem);
