3894dda9aa9097cd83b74a743797305445eec9d4
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-23 22:04:37 -0600

    vid107: wired up expense form. can now add a complete expense, has error checking, and redirects to the dashboard

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
     <h1>ADD EXPENSE</h1>
-    <ExpenseForm />
+    <ExpenseForm 
+      onSubmit={(expense) => {
+        //console.log(expense);
+        props.dispatch(addExpense(expense));   // ADDING EXPENSE TO STORE
+        props.history.push('/');
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
