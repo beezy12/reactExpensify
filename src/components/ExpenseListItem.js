7ea35272cbbb0a8ea58e08c85d7d02064d6d9df9
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-23 23:11:07 -0600

    vid109: can now edit an expense. moved 'remove expense' button into the edit page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt, note }) => (
+
+const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>{amount} - {createdAt}</p>
     <p>{note}</p>
     <p>id here {id}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }));
-    }}>REMOVE EXPENSE</button>
   </div>
 );
 
 // dont have to mapStateToProps here because I just need access to the 'dispatch' prop, which this gives me
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
