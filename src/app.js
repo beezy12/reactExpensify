553b71cd0db89f5643dd8d68b51ab75c0b26ec23
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-26 20:44:48 -0600

    vid131: removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,24 +13,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
 
-// dispatch actions to the store
-store.dispatch(expenseActions.addExpense({ description: 'Water bill', note: 'this was late', amount: 5000 }));
-store.dispatch(expenseActions.addExpense({ description: 'Gas bill', note: 'on time', createdAt: 1000 }));
-store.dispatch(expenseActions.addExpense({ description: 'car payment', note: 'this was late', amount: 666600000 }));
-// store.dispatch(filterActions.setTextFilter('bill'));   // this returns two bills
-// store.dispatch(filterActions.setTextFilter('water'));    // ....but this returns only one
-
-// setTimeout(() => {
-//   store.dispatch(filterActions.setTextFilter('bill'));
-// }, 3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-
-console.log(store.getState());
-
 // IMPORTANT - this is how you connect react to redux. wrap the app with Provider tags.
 // AppRouter used to be in the ReactDOM.render()
 const jsx = (
