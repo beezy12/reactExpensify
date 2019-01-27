f84779be584e36db2f14d7dcf5f72201f09c3d26
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-01-26 19:42:56 -0600

    vid124: testing AddExpense page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,17 +3,33 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1>ADD EXPENSE</h1>
-    <ExpenseForm 
-      onSubmit={(expense) => {
-        //console.log(expense);
-        props.dispatch(addExpense(expense));   // ADDING EXPENSE TO STORE
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    //console.log(expense);
 
-export default connect()(AddExpensePage);
+    // added the onSubmit for unit testing
+    // props.dispatch(addExpense(expense));   // ADDING EXPENSE TO STORE
+    this.props.onSubmit(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>ADD EXPENSE</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>     
+    );
+  }
+}
+
+// set all this up for unit testing
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSubmit: (expense) => dispatch(addExpense(expense))
+  };
+};
+
+// added undefined instead of mapStateToProps for the unit test
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
